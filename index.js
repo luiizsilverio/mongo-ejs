a95a0e0a1620c35a01bda9db2cf6dcd888b44c0b
luiizsilverio <luiiz.silverio@gmail.com>
2023-09-29 20:02:50 -0300

    uso de ejs

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import path from "path";
+// import path from "path";
+// import ejs from "ejs";
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.set('view engine', 'ejs');
+// server.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static("public"));
 
-
-
 app.get('/', (req, res) => {
-    res.send('APNP ✨');
+    res.render('index.ejs', {});
+})
+
+app.get('/usuarios', (req, res) => {
+    res.render('users.ejs', { usuarios: [
+      {nome: 'Diego', email: 'diego@gmail.com'},
+      {nome: 'Maria', email: 'maria@gmail.com'},
+      {nome: 'Tereza', email: 'tereza@gmail.com'},
+      {nome: 'Ana', email: 'ana@gmail.com'},
+      {nome: 'Luiz', email: 'luiz@gmail.com'},
+      {nome: 'José', email: 'jose@gmail.com'}
+    ] });
 })
 
 app.listen(3000, () => {
